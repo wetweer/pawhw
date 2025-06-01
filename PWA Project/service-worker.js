bacb969ff35dba3b316f6aeacb6e06fe18ef38cc
wetweer <wetweer77@gmail.com>
2025-06-01 17:52:56 +0300

    Add files via upload

---
+++ PWA Project/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'rentcar-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Caching app shell');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
